47261792c7e697ad760f2dcb50d2acb61f4ced86
Henaron90 <your-email@example.com>
2025-07-06 19:32:02 +0100

    i updated all the pages

--- scripts.js
+++ scripts.js
@@ -379,7 +379,7 @@ function selectAnswer(index) {
 document.addEventListener("DOMContentLoaded", () => {
   if (document.getElementById("question-text")) {
     loadQuestion();
-    startCountdown(3600); // One hour = 3600 seconds
+    startCountdown(600); // one minute = 600 seconds
 
     document.getElementById("next-btn").addEventListener("click", () => {
       if (currentQuestionIndex < questions.length - 1) {
